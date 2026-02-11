476ff5d775db41101587116fb4deefb5f38469fe
KoonnamNonpawit <koonnam148@gmail.com>
2026-02-11 11:25:54 +0700

    All Finished

---
+++ cell06/ex04/methods_everywhere.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+
+def shrink(s):
+    print(s[:8])
+
+def enlarge(s):
+    padding = 8 - len(s)
+    print(s + 'Z' * padding)
+
+if len(sys.argv) < 2:
+    print("none")
+else:
+    for arg in sys.argv[1:]:
+        if len(arg) > 8:
+            shrink(arg)
+        elif len(arg) < 8:
+            enlarge(arg)
+        else:
+            print(arg)
---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def add_one(parameter):
+    parameter += 1
+
+my_number = 0
+
+print(my_number)
+
+add_one(my_number)
+
+print(my_number)
